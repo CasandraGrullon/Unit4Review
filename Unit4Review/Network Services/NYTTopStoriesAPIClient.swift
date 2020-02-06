3ed911568adb9a2f1f0e2b0c28a8293851ff8770
Casandra <casandragrullon@pursuit.org>
2020-02-06 12:49:27 -0500

    set up APIClient

--- Unit4Review/Network Services/NYTTopStoriesAPIClient.swift
+++ Unit4Review/Network Services/NYTTopStoriesAPIClient.swift
@@ -9,4 +9,33 @@
 import Foundation
 import NetworkHelper
 
-
+struct NYTTopStoriesAPIClient {
+    static func fetchTopStories(for section: String, completion: @escaping (Result<[Article], AppError>) -> ()) {
+        
+        let endpointURL = "https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key=\(Config.apiKey)"
+        
+        guard let url = URL(string: endpointURL) else {
+            completion(.failure(.badURL(endpointURL)))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let topStories = try JSONDecoder().decode(TopStories.self, from: data)
+                    completion(.success(topStories.results))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+            
+        }
+        
+    }
+}
